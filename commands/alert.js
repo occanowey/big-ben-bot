6882801a477a3c6a34c67b7daeb52c9320f82a60
Griffin T <a12345678910ster+git@gmail.com>
2021-06-28 04:25:09 +0800

    cleanup alert command

--- commands/alert.js
+++ commands/alert.js
@@ -1,48 +1,41 @@
-const {MessageEmbed} = require('discord.js');
-const {logerrs, sendtmp} = require("../util/utils");
+const { MessageEmbed } = require("discord.js");
+const { logerrs, sendtmp, query } = require("../util/utils");
+
+const ALLOWED_USER_IDS = ["704094587836301392", "759247388606070794"];
 
 module.exports = {
-    name: 'alert',
-    description: 'Send out an alert.',
-    aliases: ['announce'],
-    async execute(client, message, args, config, db){
-        const array = ['704094587836301392', '759247388606070794']
+    name: "alert",
+    description: "Send out an alert.",
+    aliases: ["announce"],
+    async execute(client, message, args, config, db) {
+        if (!ALLOWED_USER_IDS.includes(message.author.id)) {
+            return;
+        }
 
-        array.forEach(async a => {
-            if(a === message.author.id) {
-                if(!args[0]) {
-                    logerrs(config, message.delete());
-                    logerrs(config, sendtmp(message.channel, 12000, "Please include an announcement in your message."));
+        if (args.length == 0) {
+            logerrs(config, message.delete());
+            logerrs(config, sendtmp(message.channel, 12000, "Please include an announcement in your message."));
 
-                    return;
-                }
+            return;
+        }
 
-                const embed = new MessageEmbed()
-                .setColor(config["main_config"].colorhex)
-                .setAuthor(`Notice from ${message.author.tag}`, `${message.author.displayAvatarURL()}`, `${config["other_configuration"].serverinvite}`)
-                .setDescription(`${args.join(" ")}`)
-                .setTimestamp()
-                .setFooter(`${config.main_config.copyright}`)
-                try { embed.setThumbnail(`${message.author.avatarURL({ dynamic: true })}`) } catch(e) {}
+        const embed = new MessageEmbed()
+            .setColor(config.main_config.colorhex)
+            .setThumbnail(`${message.author.avatarURL({ dynamic: true })}`)
+            .setAuthor(`Notice from ${message.author.tag}`, `${message.author.displayAvatarURL()}`, `${config.other_configuration.serverinvite}`)
+            .setDescription(`${args.join(" ")}`)
+            .setTimestamp()
+            .setFooter(`${config.main_config.copyright}`);
 
-                db.query(`SELECT * FROM guilds`, async (err, rows) => {
-                    if(err) throw err;
-                    for(let data of rows) {
-
-                        let channel = await client.channels.cache.get(data.logs)
-
-                        if(channel != 'none') {
-                            try {
-                                channel.send(embed).catch(e => {});
-                            } catch(e) {}
-                        }
-
-                    }
-
-                    logerrs(config, message.delete());
-                    logerrs(config, sendtmp(message.channel, 12000, "I have posted your announcement in all available channels."));
-                });
+        let rows = await query(db, "SELECT * FROM guilds;");
+        for (let data of rows) {
+            let channel = await client.channels.cache.get(data.logs);
+            if (channel != "none") {
+                await channel.send(embed);
             }
-        });
+        }
+
+        logerrs(config, message.delete());
+        logerrs(config, sendtmp(message.channel, 12000, "I have posted your announcement in all available channels."));
     },
 }
